8e676f95d81b3e817de24ca9351ca5066ce053ff
minki0415 <minki5050@gmail.com>
2022-08-08 13:43:08 +0900

    add new files

--- FindString.java
+++ FindString.java
@@ -1,6 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class FindString {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toLowerCase();
+        Character c = br.readLine().toLowerCase().charAt(0);
+
+        int count = 0;
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == c) count++;
+        }
+        System.out.println(count);
 
     }
 }
