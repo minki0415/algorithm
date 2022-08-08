4031c6f3e8dc91a6a111eea3e32e764d58309550
minki0415 <minki5050@gmail.com>
2022-08-08 18:33:24 +0900

    add new files

---
+++ ValidPalindrome.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ValidPalindrome {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toUpperCase().replaceAll("[^A-Z]", "");
+
+        String tmp = new StringBuilder(str).reverse().toString();
+
+        String answer = "NO";
+        if(str.equals(tmp)) answer = "YES";
+
+        System.out.println(answer);
+
+
+    }
+}
