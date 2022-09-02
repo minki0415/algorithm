0896c7e431a7e0635b36ac593e76f81ccc21ba69
minki0415 <minki5050@gmail.com>
2022-09-02 11:24:42 +0900

    add new files

---
+++ bfs/ShortestDistance2.java
@@ -0,0 +1,36 @@
+package bfs;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestDistance2 {
+    Node root;
+
+    public int bfs(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        int L = 0;
+        while(!q.isEmpty()) {
+            int len = q.size();
+            for(int i = 0; i < len ; i++) {
+                Node x = q.poll();
+                if(x.lt != null) q.offer(x.lt);
+                if(x.rt != null) q.offer(x.rt);
+                if(x.lt == null && x.rt == null) return L;
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ShortestDistance2 tree = new ShortestDistance2();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.bfs(tree.root));
+    }
+}
